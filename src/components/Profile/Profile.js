876260b53f222e65f02d6200b105b077564df9f7
saran-1916 <saran.sp@yahoo.com>
2026-01-28 23:57:38 +0530

    Add files via upload

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="text-gray-800 text-xl font-semibold">
+      Welcome to your profile!
+    </div>
+  );
+};
+
+export default Profile;
