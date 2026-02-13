93c3efe28061c0b961fa351f3f701a002e9d4981
saran-1916 <saran.sp@yahoo.com>
2026-02-14 00:21:26 +0530

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Profile = () => {
   return (
     <div className="text-gray-800 text-xl font-semibold">
-      Welcome to your profile!
+      Welcome to your profile, Master
     </div>
   );
 };
+
 
 export default Profile;
